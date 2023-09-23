24712c332020dde17614b648a11c8c222945403b
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-09-23 14:57:00 -0700

    CreatePages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Menu from "./components/Menu/Menu";
 import Main from "./components/Main/Main";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
---
+++ src/pages/dough/Dough.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DoughPage() {
+  return <h1>Dough Page</h1>;
+}
+
+export default DoughPage;
