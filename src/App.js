02e667f1f6af363fa266703b90b7fc1e21651af2
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-09-23 15:04:55 -0700

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React from "react";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Dough from "./pages/dough/Dough";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Menu from "./components/Menu/Menu";
 import Main from "./components/Main/Main";
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/dough">Dough</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/about" Component={About} />
+          <Route path="/contact" Component={Contact} />
+          <Route path="/dough" Component={Dough} />
+        </div>
+      </Router>
+
       <Main />
       <Menu />
     </>
